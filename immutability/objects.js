eabfafa38c1cbf40c99525823d888fbd63d98cf6
TK <leandrotk100@gmail.com>
2021-12-27 16:25:11 -0300

    immutability

---
+++ immutability/objects.js
@@ -0,0 +1,26 @@
+const objectA = {
+  text: 'text',
+  number: 1,
+  active: true,
+  behaviour: () => {
+    console.log('doing!');
+  }
+}
+
+const objectB = {
+  text: 'text',
+  number: 1,
+  active: true,
+  behaviour: () => {
+    console.log('doing!');
+  }
+}
+
+console.log('objectA == objectB   -->', objectA == objectB);
+console.log('objectA == objectA   -->', objectA == objectA);
+console.log('objectA === objectB  -->', objectA === objectB);
+console.log('objectA === objectA  -->', objectA === objectA);
+
+objectA.active = false;
+console.log('objectA == objectA   -->', objectA == objectA);
+console.log('objectA === objectA  -->', objectA === objectA);
